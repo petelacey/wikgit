ac0f17d03cd45136424c199d85ca7e8da893a473
Pete Lacey <peter.lacey@sonian.net>
2011-02-27 10:47:59 -0500

    Add support for external configuration files

--- wikgit.rb
+++ wikgit.rb
@@ -1,4 +1,13 @@
-class Wikgit < Sinatra::Base
+class WikgitApp < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  configure do
+    yaml= YAML.load_file("#{settings.root}/config/environments/#{settings.environment}.yaml") rescue {}
+    yaml.each_pair do |key, value|
+      set key, value unless methods(false).any? { |m| m.to_s == key.to_s }
+    end
+  end
+
   get "/" do
     "Hi"
   end
